a8c7f50edafe43f498b1051f11ceef0e48914a92
Prudnikova <dakoto4ka@yandex.ru>
2023-02-01 14:27:11 +0300

    ДЗ1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import task1.Products;
+import task1.ProductsList;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
     }
 }
